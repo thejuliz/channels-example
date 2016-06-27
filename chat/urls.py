2e706135d42adeaad9ce566f9b2c08d9697abd56
Julapat Julnual <thejuliz@gmail.com>
2016-06-27 14:54:51 -0700

    fix urls

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$',  views.about, name='about'),
     url(r'^new/$', views.new_room, name='new_room'),
     #url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
-    url(r'^chat/$', chat),
+    url(r'^chat/$', views.chat),
 ]
